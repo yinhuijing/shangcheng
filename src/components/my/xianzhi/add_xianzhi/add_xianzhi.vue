92e777cbfd148441bd89167b3803de6c72a1023a
Yyqx521 <1105340970@qq.com>
2020-10-27 17:45:36 +0800

    sixth

--- src/components/my/xianzhi/add_xianzhi/add_xianzhi.vue
+++ src/components/my/xianzhi/add_xianzhi/add_xianzhi.vue
@@ -140,6 +140,24 @@
           </div>
         </div>
 
+        <!-- <fieldset
+          class="layui-elem-field layui-field-title"
+          style="margin-top: 30px"
+        >
+          <legend>上传多张图片</legend>
+        </fieldset>
+
+        <div class="layui-upload">
+          <button type="button" class="layui-btn" id="test2">多图片上传</button>
+          <blockquote
+            class="layui-elem-quote layui-quote-nm"
+            style="margin-top: 10px"
+          >
+            预览图：
+            <div class="layui-upload-list" id="demo2"></div>
+          </blockquote>
+        </div> -->
+
         <div class="layui-upload upload">
           <button type="button" class="layui-form-label" id="upload">
             上传图片
@@ -150,6 +168,26 @@
             <p id="demoText"></p>
           </div>
         </div>
+
+        <!-- <div class="layui-form-item">
+          <label class="layui-form-label">闲置物品详情：</label>
+          <div class="layui-input-inline uploadHeadImage">
+            <div class="layui-upload-drag" id="headImg">
+              <i class="layui-icon"></i>
+              <p>点击上传图片，或将图片拖拽到此处</p>
+            </div>
+          </div>
+          <div class="layui-input-inline">
+            <div class="layui-upload-list">
+              <img
+                class="layui-upload-img headImage"
+                src="http://t.cn/RCzsdCq"
+                id="demo1"
+              />
+              <p id="demoText"></p>
+            </div>
+          </div>
+        </div> -->
 
         <div class="layui-form-item layui-form-text">
           <label class="layui-form-label">闲置理由：</label>
@@ -224,7 +262,6 @@ export default {
       var uploadInst = upload.render({
         elem: "#upload",
         url: "/User/uploadImg", //改成您自己的上传接口
-        methods: "post",
         before: function (obj) {
           //预读本地文件示例，不支持ie8
           obj.preview(function (index, file, result) {
@@ -247,6 +284,67 @@ export default {
           demoText.find(".demo-reload").on("click", function () {
             uploadInst.upload();
           });
+        },
+      });
+
+      // var uploadInst = upload.render({
+      //   elem: "#headImg",
+      //   url: "/User/uploadImg",
+      //   size: 500,
+      //   before: function (obj) {
+      //     //预读本地文件示例，不支持ie8
+      //     obj.preview(function (index, file, result) {
+      //       $("#demo1").attr("src", result); //图片链接（base64）
+      //     });
+      //   },
+      //   done: function (res) {
+      //     //如果上传失败
+      //     if (res.code > 0) {
+      //       return layer.msg("上传失败");
+      //     }
+      //     //上传成功
+      //     //打印后台传回的地址: 把地址放入一个隐藏的input中, 和表单一起提交到后台, 此处略..
+      //     /*   console.log(res.data.src);*/
+      //     window.parent.uploadHeadImage(res.data.src);
+      //     var demoText = $("#demoText");
+      //     demoText.html('<span style="color: #8f8f8f;">上传成功!!!</span>');
+      //   },
+      //   error: function () {
+      //     //演示失败状态，并实现重传
+      //     var demoText = $("#demoText");
+      //     demoText.html(
+      //       '<span style="color: #FF5722;">上传失败</span> <a class="layui-btn layui-btn-mini demo-reload">重试</a>'
+      //     );
+      //     demoText.find(".demo-reload").on("click", function () {
+      //       uploadInst.upload();
+      //     });
+      //   },
+      // });
+      // element.init();
+
+      // 多图片上传
+      upload.render({
+        elem: "#test2",
+        url: "/User/uploadImg", //自己的上传接口
+        methods: "post",
+        // data: { file:this.file }, // 接口参数
+        multiple: true,
+        before: function (obj) {
+          //预读本地文件示例
+          obj.preview(function (index, file, result) {
+            console.log(file);
+            $("#demo2").append(
+              '<img src="' +
+                result +
+                '" alt="' +
+                file.name +
+                '" class="layui-upload-img" style="display:block;width:4rem;height:4rem;float:left;">'
+            );
+          });
+          console.log(obj);
+        },
+        done: function (res) {
+          //上传完毕
         },
       });
     });
